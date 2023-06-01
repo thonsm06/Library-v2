770d3efe0d2529a53acd73bacb881c1ce33d1cf4
Smith Thongbai <thonsm06@gmail.com>
2023-06-01 13:23:39 -0500

    Move styling to style.css

--- script.js
+++ script.js
@@ -1,47 +1,58 @@
 const body = document.querySelector('body');
-const library = document.createElement('div');
+const mainContainer = document.createElement('div'); //holds everything inside the body
+const header = document.createElement('div');
+const library = document.createElement('div');       //holds main content
 
-library.style.cssText = 'display: grid; grid-template-columns: repeat(auto-fill, minmax(100px, 20%)); gap: 32px;';
-
-body.appendChild(library);
+mainContainer.classList.toggle('main-container');
+header.classList.toggle('header');
+library.classList.toggle('library');
 
 let myLibrary = [];
 
-function Book(title, author) {
+function Book(title, author, page, read) {
     this.title = title;
     this.author = author;
+    this.page = page;
+    this.read = read;
 }
 
-function addBookToLibrary(title, author) {
-    const book = new Book(title, author);
+function addBookToLibrary(title, author, page, read) {
+    const book = new Book(title, author, page, read);
     myLibrary.push(book);
 }
 
-addBookToLibrary('Think Like a Programmer', 'V. Anton Spraul');
-addBookToLibrary('Lord of the Ring', 'J. R. R. Tolkien');
+addBookToLibrary('Think Like a Programmer', 'V. Anton Spraul', '233', 'true');
+addBookToLibrary('Lord of the Rings', 'J.R.R. Tolkien', '1178', 'false');
+addBookToLibrary('Harry Potter and the Sorcerer\'s Stone', 'J.K. Rowling', '309', 'false');
 
 myLibrary.forEach(book => {
-    const bookContainer = document.createElement('div');
+    const bookContainer = document.createElement('div'); //hold individual book contents
     const title = document.createElement('p');
     const author = document.createElement('p');
+    const page = document.createElement('p');
+    const read = document.createElement('p');
 
-    bookContainer.style.cssText = 'background-color: rgb(0, 0, 0, 0.2); border: 3px solid black; border-radius: 10px; text-align: center';
-    title.style.cssText = 'color: white; font-size: 2rem;';
-    author.style.cssText = 'color: white; font-size: 1.5rem;';
-
+    bookContainer.classList.toggle('book');
+    title.classList.toggle('title');
+    author.classList.toggle('author');
+    page.classList.toggle('page');
+    read.classList.toggle('read');
+    
     title.textContent = book.title;
     author.textContent = book.author;
+    page.textContent = book.page;
 
-    bookContainer.appendChild(title);
-    bookContainer.appendChild(author);
+    bookContainer.append(title, author, page, read);
+
     library.appendChild(bookContainer);
 })
 
 const addButton = document.createElement('button');
+addButton.classList.toggle('addBtn');
 addButton.style.cssText = 'background-color: #44CF85; color: white; border: none; border-radius: 40px; font-size: 3rem; padding: 0 40px';
 addButton.textContent = '+';
 
-addButton.addEventListener('click', () => {
+addButton.addEventListener('click', event => {
     const form = document.createElement('form');
     const labelTitle = document.createElement('label');
     const inputTitle = document.createElement('input');
@@ -51,12 +62,14 @@ addButton.addEventListener('click', () => {
     const inputPage = document.createElement('input');
     const labelRead = document.createElement('label');
     const inputRead = document.createElement('input');
+    const submitButton = document.createElement('button');
 
     labelTitle.textContent = 'Title: ';
     labelAuthor.textContent = 'Author: ';
     labelPage.textContent = 'Page Number: ';
     labelRead.textContent = 'Read?';
-    
+    submitButton.textContent = 'ADD';
+
     labelTitle.htmlFor = 'title';
     labelAuthor.htmlFor = 'author';
     labelPage.htmlFor = 'page';
@@ -66,6 +79,7 @@ addButton.addEventListener('click', () => {
     inputAuthor.type = 'text';
     inputPage.type = 'text';
     inputRead.type = 'checkbox';
+    submitButton.type = 'submit';
 
     inputTitle.id = 'title';
     inputAuthor.id = 'author';
@@ -77,8 +91,13 @@ addButton.addEventListener('click', () => {
     inputPage.name = 'page';
     inputRead.name = 'read';
 
-    form.append(labelTitle, inputTitle, labelAuthor, inputAuthor, labelPage, inputPage, labelRead, inputRead);
+    form.append(labelTitle, inputTitle, labelAuthor, inputAuthor, labelPage, inputPage, labelRead, inputRead, submitButton);
     body.appendChild(form);
+
+    event.preventDefault();
 })
 
-body.appendChild(addButton);
+
+header.appendChild(addButton);
+mainContainer.append(header, library);
+body.appendChild(mainContainer);

ccee3dacdb7fb9e89d5ace5eb295d7e3d9bcfc6a
Smith Thongbai <thonsm06@gmail.com>
2023-06-01 10:52:51 -0500

    Added function to loop through the library content and list them on the page.

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const body = document.querySelector('body');
+const library = document.createElement('div');
+
+library.style.cssText = 'display: grid; grid-template-columns: repeat(auto-fill, minmax(100px, 20%)); gap: 32px;';
+
+body.appendChild(library);
+
 let myLibrary = [];
 
 function Book(title, author) {
@@ -7,9 +14,27 @@ function Book(title, author) {
 
 function addBookToLibrary(title, author) {
     const book = new Book(title, author);
-    myLibrary[0] = book;
+    myLibrary.push(book);
 }
 
-addBookToLibrary('lotr', 'j.r.r token');
+addBookToLibrary('Think Like a Programmer', 'V. Anton Spraul');
+addBookToLibrary('Lord of the Ring', 'J. R. R. Tolkien');
 
-console.log(myLibrary);
+myLibrary.forEach(book => {
+    const bookContainer = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p');
+
+    bookContainer.style.cssText = 'background-color: rgb(0, 0, 0, 0.2); border: 3px solid black; border-radius: 10px; text-align: center';
+    title.style.cssText = 'color: white; font-size: 24px;';
+    author.style.cssText = 'color: white; font-size: 18px;';
+
+    title.textContent = book.title;
+    author.textContent = book.author;
+
+    bookContainer.appendChild(title);
+    bookContainer.appendChild(author);
+    library.appendChild(bookContainer);
+})
+
+console.log(myLibrary)

cef5c883d9b6ea30953d6ad76d86dc09dd2b6086
Smith Thongbai <thonsm06@gmail.com>
2023-06-01 16:26:04 -0500

    Added container divs to inputs and labels.

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ mainContainer.classList.toggle('main-container');
 header.classList.toggle('header');
 library.classList.toggle('library');
 
+
+
 let myLibrary = [];
 
 function Book(title, author, page, read) {
@@ -64,58 +66,102 @@ const addButton = document.createElement('button');
 addButton.classList.toggle('add-button');
 addButton.textContent = '+';
 
+let formOpen = false;
 addButton.addEventListener('click', event => {
-    const form = document.createElement('form');
-    const labelTitle = document.createElement('label');
-    const inputTitle = document.createElement('input');
-    const labelAuthor = document.createElement('label');
-    const inputAuthor = document.createElement('input');
-    const labelPage = document.createElement('label');
-    const inputPage = document.createElement('input');
-    const labelRead = document.createElement('label');
-    const inputRead = document.createElement('input');
+    if (formOpen === false) {
+        formOpen = true;
+        const form = document.createElement('form');
+        const divTitle = document.createElement('div');
+        const labelTitle = document.createElement('label');
+        const inputTitle = document.createElement('input');
+        const divAuthor = document.createElement('div');
+        const labelAuthor = document.createElement('label');
+        const inputAuthor = document.createElement('input');
+        const divPage = document.createElement('div');
+        const labelPage = document.createElement('label');
+        const inputPage = document.createElement('input');
+        const divRead = document.createElement('div');
+        const labelRead = document.createElement('label');
+        const inputRead = document.createElement('input');
 
-    form.classList.toggle('form');
+        form.classList.toggle('form');
 
-    labelTitle.textContent = 'Title: ';
-    labelAuthor.textContent = 'Author: ';
-    labelPage.textContent = 'Page Number: ';
-    labelRead.textContent = 'Read?';
+        labelTitle.textContent = 'Title ';
+        labelAuthor.textContent = 'Author ';
+        labelPage.textContent = 'Page Number ';
+        labelRead.textContent = 'Read';
+
+        labelTitle.htmlFor = 'title';
+        labelAuthor.htmlFor = 'author';
+        labelPage.htmlFor = 'page';
+        labelRead.htmlFor = 'read';
+
+        inputTitle.required = true;
+        
+        inputTitle.type = 'text';
+        inputAuthor.type = 'text';
+        inputPage.type = 'text';
+        inputRead.type = 'checkbox';
 
 
-    labelTitle.htmlFor = 'title';
-    labelAuthor.htmlFor = 'author';
-    labelPage.htmlFor = 'page';
-    labelRead.htmlFor = 'read';
+        inputTitle.id = 'title';
+        inputAuthor.id = 'author';
+        inputPage.id = 'page';
+        inputRead.id = 'read';
 
-    inputTitle.type = 'text';
-    inputAuthor.type = 'text';
-    inputPage.type = 'text';
-    inputRead.type = 'checkbox';
+        inputTitle.name = 'title';
+        inputAuthor.name = 'author';
+        inputPage.name = 'page';
+        inputRead.name = 'read';
 
+        inputTitle.focus();
+        inputTitle.select();
+        console.log(inputTitle);
+        const divButton = document.createElement('div');
+        const submitForm = document.createElement('button');
+        submitForm.textContent = 'ADD';
+        submitForm.type = 'submit';
+        submitForm.addEventListener('click', event => {
+            if (inputTitle.value != "") {
+                addBookToLibrary(inputTitle.value, inputAuthor.value, inputPage.value, inputRead.value);
+                inputTitle.value = '';
+                inputAuthor.value = '';
+                inputPage.value = '';
+                inputRead.checked = false;
+                
+            }
+            event.preventDefault();
+        });
 
-    inputTitle.id = 'title';
-    inputAuthor.id = 'author';
-    inputPage.id = 'page';
-    inputRead.id = 'read';
+        const resetForm = document.createElement('button');
+        resetForm.textContent = 'Reset';
+        resetForm.type = 'reset';
+        resetForm.addEventListener('click', event => {
+            
+        })
 
-    inputTitle.name = 'title';
-    inputAuthor.name = 'author';
-    inputPage.name = 'page';
-    inputRead.name = 'read';
+        divTitle.append(labelTitle, inputTitle);
+        divAuthor.append(labelAuthor, inputAuthor);
+        divPage.append(labelPage, inputPage);
+        divRead.append(labelRead, inputRead);
+        divButton.append(submitForm, resetForm);
+        form.append(divTitle, divAuthor, divPage, divRead, divButton);
+        header.appendChild(form);
+    } else {
+        formOpen = false;
+        const form = document.querySelector('form');
+        form.remove();
+    }
 
-    const submitButton = document.createElement('button');
-    submitButton.textContent = 'ADD';
-    submitButton.type = 'submit';
-
-    submitButton.addEventListener('click', event => {
-        addBookToLibrary(inputTitle.value, inputAuthor.value, inputPage.value, inputRead.value);
-        event.preventDefault();
-    });
-
-    form.append(labelTitle, inputTitle, labelAuthor, inputAuthor, labelPage, inputPage, labelRead, inputRead, submitButton);
-    body.appendChild(form);
 })
+
+/* body.addEventListener('click', () => {
+    if (formOpen === true) {
+        formOpen = false;
+        const form = document.querySelector('form');
+        form.remove();
+    }
+}) */
 
 header.appendChild(addButton);
 mainContainer.append(header, library);

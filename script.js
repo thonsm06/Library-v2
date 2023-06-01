9c3694fa392dd4db441811e62505b9d0839f3af1
Smith Thongbai <thonsm06@gmail.com>
2023-06-01 11:21:39 -0500

    Add a button to add new book into the library. The button create a form for the user to input the book infomation.

--- script.js
+++ script.js
@@ -26,8 +26,8 @@ myLibrary.forEach(book => {
     const author = document.createElement('p');
 
     bookContainer.style.cssText = 'background-color: rgb(0, 0, 0, 0.2); border: 3px solid black; border-radius: 10px; text-align: center';
-    title.style.cssText = 'color: white; font-size: 24px;';
-    author.style.cssText = 'color: white; font-size: 18px;';
+    title.style.cssText = 'color: white; font-size: 2rem;';
+    author.style.cssText = 'color: white; font-size: 1.5rem;';
 
     title.textContent = book.title;
     author.textContent = book.author;
@@ -37,4 +37,48 @@ myLibrary.forEach(book => {
     library.appendChild(bookContainer);
 })
 
-console.log(myLibrary)
+const addButton = document.createElement('button');
+addButton.style.cssText = 'background-color: #44CF85; color: white; border: none; border-radius: 40px; font-size: 3rem; padding: 0 40px';
+addButton.textContent = '+';
+
+addButton.addEventListener('click', () => {
+    const form = document.createElement('form');
+    const labelTitle = document.createElement('label');
+    const inputTitle = document.createElement('input');
+    const labelAuthor = document.createElement('label');
+    const inputAuthor = document.createElement('input');
+    const labelPage = document.createElement('label');
+    const inputPage = document.createElement('input');
+    const labelRead = document.createElement('label');
+    const inputRead = document.createElement('input');
+
+    labelTitle.textContent = 'Title: ';
+    labelAuthor.textContent = 'Author: ';
+    labelPage.textContent = 'Page Number: ';
+    labelRead.textContent = 'Read?';
+    
+    labelTitle.htmlFor = 'title';
+    labelAuthor.htmlFor = 'author';
+    labelPage.htmlFor = 'page';
+    labelRead.htmlFor = 'read';
+
+    inputTitle.type = 'text';
+    inputAuthor.type = 'text';
+    inputPage.type = 'text';
+    inputRead.type = 'checkbox';
+
+    inputTitle.id = 'title';
+    inputAuthor.id = 'author';
+    inputPage.id = 'page';
+    inputRead.id = 'read';
+
+    inputTitle.name = 'title';
+    inputAuthor.name = 'author';
+    inputPage.name = 'page';
+    inputRead.name = 'read';
+
+    form.append(labelTitle, inputTitle, labelAuthor, inputAuthor, labelPage, inputPage, labelRead, inputRead);
+    body.appendChild(form);
+})
+
+body.appendChild(addButton);
